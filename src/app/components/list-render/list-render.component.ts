5e1ff2d3511cf09c51e0970a1b96300f4bd0e7c2
danillobr <danillorodriguesabreu@gmail.com>
2024-06-18 15:57:28 -0300

    feat: creating interfaces for entities

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,21 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Animal } from 'src/app/Animal';
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css']
 })
 export class ListRenderComponent implements OnInit {
-  animals = [
-    {name: "Turca", type: "Dog"},
-    {name: "Tom", type: "Cat"},
-    {name: "Jerry", type: "Mouse"},
-    {name: "Piu-piu", type: "Bird"},
+  animals: Animal[] = [
+    {name: "Turca", type: "Dog", age: 10},
+    {name: "Tom", type: "Cat", age: 5},
+    {name: "Jerry", type: "Mouse", age: 2},
+    {name: "Piu-piu", type: "Bird", age: 2},
   ];
+
+  animal: Animal = {name: "Kiko", type: "Cat", age: 4}
+
+  animalDetails = "";
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showAge(animal: Animal){
+    this.animalDetails = `O pet ${animal.name} tem  ${animal.age} anos!`;
   }
 
 }
